21f977a31ad62ca1f2ee905378a3d9879f9a0550
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-11 08:53:43 +0100

    code added

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const progress = document.getElementById('progress')
 const song = document.getElementById('song')
 const ctrlIcon = document.getElementById('ctrl-icon')
+
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause() {
+    if(ctrlIcon.classList.contains('fa-pause')) {
+        song.pause();
+        ctrlIcon.classList.remove('fa-pause')
+        ctrlIcon.classList.add('fa-play')
+    } else {
+        song.play()
+        ctrlIcon.classList.add('fa-pause')
+        ctrlIcon.classList.remove('fa-play')
+    }
+}
